c6ae58b978d941bc116246b356d32bb4c852bc28
pythonwby <2147474866@qq.com>
2024-08-08 21:25:29 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import listToImage from './SeatChangerCore';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [seatImage, setSeatImage] = useState(listToImage());
+	return (
+		<>
+			<Container maxWidth='sm'>
+				<Box sx={{bgcolor: '#cfe8fc'}}>
+					<Grid container spacing={1} alignContent={'center'} justifyContent={'center'}>
+						<Grid item xs={12}>
+							<TableContainer component={Paper}>
+								<Table sx={{minWidth: 30}}>
+									<TableBody>
+										{seatImage.map((seat)=>(
+											<TableRow>
+												<TableCell>{seat[0]}</TableCell>
+												<TableCell>{seat[1]}</TableCell>
+												<TableCell>{seat[2]}</TableCell>
+												<TableCell>{seat[3]}</TableCell>
+												<TableCell>{seat[4]}</TableCell>
+												<TableCell>{seat[5]}</TableCell>
+												<TableCell>{seat[6]}</TableCell>
+												<TableCell>{seat[7]}</TableCell>
+												<TableCell>{seat[8]}</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</Grid>
+					</Grid>
+				</Box>
+			</Container>
+		</>
+	);
 }
 
 export default App;
