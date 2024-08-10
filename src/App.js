b6f0fd92f8448419081458899e7be8232cd40b81
pythonwby <2147474866@qq.com>
2024-08-10 16:41:05 +0800

    init

--- src/App.js
+++ src/App.js
@@ -21,11 +21,13 @@ import { IconButton } from '@mui/material';
 import { listToImage, getWeekString, nextWeekShift, lastWeekShift, shiftToNow } from './SeatChangerCore';
 import './Styles.css';
 
+const timeNow = new Date();
+const lightOrDark = 16;
 const darkTheme = createTheme({palette:{mode: 'dark'}});
 const lightTheme = createTheme({palette:{mode: 'light'}});
-const tablecellStyle = {fontFamily: 'LXGWWenkaiGB', fontSize: 15.3, whiteSpace: 'nowrap', border: '1px dashed gray', padding: '12px 15px', bgcolor: 'rgba(0,0,0,0)'};
-const emptyTablecellStyle = {fontFamily: 'LXGWWenkaiGB', fontSize: 15, whiteSpace: 'nowrap', border: 0, width: 8};
-const buttonStyle = {fontFamily: 'LXGWWenkaiGB', fontSize: 16, fontWeight: 'bold', height: 33, border: 2}
+const tablecellStyle = {fontFamily: 'LXGW WenKai Lite', fontSize: 15.3, whiteSpace: 'nowrap', border: '1px dashed gray', padding: '12px 15px', bgcolor: 'rgba(0,0,0,0)'};
+const emptyTablecellStyle = {fontFamily: 'LXGW WenKai Lite', fontSize: 15, whiteSpace: 'nowrap', border: 0, width: 8};
+const buttonStyle = {fontFamily: 'LXGW WenKai Lite', fontSize: 16, fontWeight: 'bold', height: 33, border: 2}
 
 // if (screenfull.isEnabled){
 // 	screenfull.request();
@@ -34,12 +36,12 @@ const buttonStyle = {fontFamily: 'LXGWWenkaiGB', fontSize: 16, fontWeight: 'bold
 function App() {
 	const [seatImage, setSeatImage] = useState(listToImage());
 	const [weekString, setWeekString] = useState(getWeekString());
-	const [colorMode, setColorMode] = useState(lightTheme);
-	const [colorModeIcon, setColorModeIcon] = useState(colorMode.palette.mode === 'dark' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />);
-	const [backgroundCssClass,setBackgroundCssClass] = useState('backgroundImage-style-light');
-	const [boxBackgroundSx, setBoxBackgroundColor] = useState({bgcolor: 'rgba(210,225,230,0.7)', width: 700});
-	const [paperBackgroundSx, setPaperBackgroundColor] = useState({width: 633, bgcolor: 'rgba(255,255,255,0.4)'});
-	const [dividerTextSx, setDividerTextSx] = useState({fontFamily: 'LXGWWenkaiGB', fontSize: 13, color: '#333333'});
+	const [colorMode, setColorMode] = useState(timeNow.getHours() < lightOrDark ? lightTheme : darkTheme);
+	const [colorModeIcon, setColorModeIcon] = useState(colorMode.palette.mode === 'dark' ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon />);
+	const [backgroundCssClass,setBackgroundCssClass] = useState(timeNow.getHours() < lightOrDark ? 'backgroundImage-style-light' : 'backgroundImage-style-dark');
+	const [boxBackgroundSx, setBoxBackgroundColor] = useState(timeNow.getHours() < lightOrDark ? {bgcolor: 'rgba(210,225,230,0.7)', width: 700} : {bgcolor: 'rgba(70,75,80,0.6)', width: 700});
+	const [paperBackgroundSx, setPaperBackgroundColor] = useState(timeNow.getHours() < lightOrDark ? {width: 633, bgcolor: 'rgba(255,255,255,0.4)'} : {width: 633, bgcolor: 'rgba(30,30,30,0.6)'});
+	const [dividerTextSx, setDividerTextSx] = useState(timeNow.getHours() < lightOrDark ? {fontFamily: 'LXGW WenKai Lite', fontSize: 13, color: '#333333'} : {fontFamily: 'LXGW WenKai Lite', fontSize: 13, color: '#BBBBBB'});
 
 	function onNextWeekButtonPress(){
 		nextWeekShift();
@@ -66,14 +68,14 @@ function App() {
 			setBackgroundCssClass('backgroundImage-style-light');
 			setBoxBackgroundColor({bgcolor: 'rgba(210,225,230,0.7)', width: 700});
 			setPaperBackgroundColor({width: 633, bgcolor: 'rgba(255,255,255,0.4)'});
-			setDividerTextSx({fontFamily: 'LXGWWenkaiGB', fontSize: 13, color: '#333333'});
+			setDividerTextSx({fontFamily: 'LXGW WenKai Lite', fontSize: 13, color: '#333333'});
 		}else if (colorMode.palette.mode === 'light') { //Light to Dark
 			setColorModeIcon(<LightModeOutlinedIcon/>);
 			setColorMode(darkTheme);
 			setBackgroundCssClass('backgroundImage-style-dark');
 			setBoxBackgroundColor({bgcolor: 'rgba(70,75,80,0.6)', width: 700});
 			setPaperBackgroundColor({width: 633, bgcolor: 'rgba(30,30,30,0.6)'});
-			setDividerTextSx({fontFamily: 'LXGWWenkaiGB', fontSize: 13, color: '#BBBBBB'});
+			setDividerTextSx({fontFamily: 'LXGW WenKai Lite', fontSize: 13, color: '#BBBBBB'});
 		}
 	}
 
@@ -117,7 +119,7 @@ function App() {
 								<Button color='info' size='small' variant='outlined' sx={buttonStyle} onClick={onLastWeekButtonPress}>上一周</Button>
 							</Grid>
 							<Grid xs={2} alignContent={'center'} justifyContent={'center'}>
-								<span class='span-style'>{weekString}</span>
+								<span className='span-style'>{weekString}</span>
 							</Grid>
 							<Grid xs={2}>
 								<Button color='info' size='small' variant='outlined' sx={buttonStyle} onClick={onNextWeekButtonPress}>下一周</Button>
@@ -131,15 +133,15 @@ function App() {
 								<Divider variant='middle' color='#888888' sx={dividerTextSx}>Powered by React and JavaScript</Divider>
 							</Grid>
 							<Grid xs={12}>
-								<span class='mobile-warning-span-style'>移动端建议横屏查看该网页</span>
-								{/* <span class='mobile-warning-span-style'>当前为内部测试版本,一切内容以正式发布为准</span> */}
+								<span className='mobile-warning-span-style'>移动端建议横屏查看该网页</span>
+								{/* <span className='mobile-warning-span-style'>当前为内部测试版本,一切内容以正式发布为准</span> */}
 							</Grid>
 							<Grid xs={12} alignContent={'center'} justifyContent={'center'}>
 								<Stack spacing={1} direction='row' alignContent={'center'} justifyContent={'center'} divider={<Divider orientation="vertical" variant='middle' flexItem />}>
-									<span class='info-span-style'>数据收集时间:2024.6.6</span>
-									<span class='info-span-style'>数据修正时间:2024.8.9</span>
-									<a href='https://github.com/pythonwby/SeatChangerReact' class='info-span-style'>GitHub</a>
-									<span class='info-span-style'>SeatChangerReact by pythonwby on 2024.8.10</span>
+									<span className='info-span-style'>数据收集时间:2024.6.6</span>
+									<span className='info-span-style'>数据修正时间:2024.8.9</span>
+									<a href='https://github.com/pythonwby/SeatChangerReact' className='info-span-style'>GitHub</a>
+									<span className='info-span-style'>SeatChangerReact by pythonwby on 2024.8.10</span>
 								</Stack>
 							</Grid>
 							<Grid xs={12} sx={{height: '1vh'}}/>
